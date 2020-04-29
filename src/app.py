ee25f02d8000d22553f8889b42fd918636bf69cc
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2020-04-30 09:33:58 +1000

    Add json error code for unavailable languages

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@
 # @Author: AnthonyKenny98
 # @Date:   2020-04-28 14:19:07
 # @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2020-04-30 09:25:41
+# @Last Modified time: 2020-04-30 09:32:49
 
 from flask import Flask, jsonify
 from datetime import datetime
@@ -15,8 +15,11 @@ app = Flask(__name__)
 @app.route('/<language>')
 def phrase_of_the_dat(language):
     """Basic Respond."""
-    with open('db/{}.txt'.format(language)) as f:
-        db = json.loads(f.read())
+    try:
+        with open('db/{}.txt'.format(language)) as f:
+            db = json.loads(f.read())
+    except FileNotFoundError:
+        return jsonify({'error': 'language not available'})
     time = datetime.combine(datetime.today(), datetime.min.time()).timestamp()
     index = int(time % len(db))
     return jsonify(db[index])
