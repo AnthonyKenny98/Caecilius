a143cd81c8b337a527453d24f5ead8afcb03b30e
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2020-04-30 10:23:34 +1000

    French Dict A done

--- scripts/load_db.py
+++ scripts/load_db.py
@@ -3,7 +3,7 @@
 # @Author: AnthonyKenny98
 # @Date:   2020-04-28 14:34:42
 # @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2020-04-30 09:44:36
+# @Last Modified time: 2020-04-30 10:20:42
 
 import sys
 from os.path import dirname, realpath
@@ -25,16 +25,14 @@ db = []
 #             })
 
 # French Input
-# https://en.wikipedia.org/wiki/Glossary_of_French_expressions_in_English
 with open(sys.argv[1], 'r') as infile:
-    lines = list(infile)
-    for i in range(int(len(lines) / 2)):
-        (french, eng) = lines[i * 2], lines[i * 2 + 1]
-        # (french, eng) = line.strip("\n").split(" - ")
-        db.append({
-            "phrase": french.strip("\n"),
-            "translation": eng.strip("\n")
-        })
+    for line in infile:
+        if line is not "\n":
+            (fre, eng) = line.strip("\n").split(" ::: ")
+            db.append({
+                "phrase": fre,
+                "translation": eng.replace('lit.', 'literal:')
+            })
 
 
 with open(DATABASE, 'w') as f:
